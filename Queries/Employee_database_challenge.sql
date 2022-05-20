a6a3ff84fd57cf95b137d70cee9b42918bdd4a6e
e-sycheung <93056479+e-sycheung@users.noreply.github.com>
2022-05-19 20:45:17 -0700

    Update Employee_database_challenge.sql

--- Queries/Employee_database_challenge.sql
+++ Queries/Employee_database_challenge.sql
@@ -17,8 +17,8 @@ SELECT DISTINCT ON (emp_no) emp_no,
 first_name,
 last_name,
 title
-FROM retirement_titles
 INTO unique_titles
+FROM retirement_titles
 WHERE to_date = '9999-01-01'
 ORDER BY emp_no, to_date DESC;
 
